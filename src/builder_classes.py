2fa8af4646d200cfe3451ae623bea2393570c91c
MatteoGravinese <matteogravinese158@gmail.com>
2025-09-26 19:07:55 -0400

    Edited Python JSON creations

--- src/builder_classes.py
+++ src/builder_classes.py
@@ -1,75 +1,99 @@
 import json
-def function():
-    class Encoding:
-        def __init__(self):
-            self._channels = {}
-        def channel(self, name):
-            self._current_channel = name
-            return self
-        def field(self, fieldsname):
-            self._channels[self._current_channel] = fieldsname
-            self._current_channel = None
-            return self
-        def build(self):
-            return self._channels
-    class Visualization:
-        def __init__(self):
-            self._mark = None
-            self._encoding = None
-        def mark(self, marktype):
-            self._mark = marktype
-            return self
-        def add_encoding(self, encoding):
-            self._encoding = encoding.build()
-            return self
-        def build(self):
-            return {
-                "type": "Visualization",
-                "mark": self._mark,
-                "encoding": self._encoding
-            }
-    class Layout:
-        def __init__(self):
-            self._direction = None
-            self._gap = None
-            self._children = []
-        def direction(self, dirn):
-            self._direction = dirn
-            return self
-        def gap(self, gap):
-            self._gap = gap
-            return self
-        def add_child(self, *children):
-            for child in children:
-                self._children.append(child.build())
-            return self
-        def build(self):
-            return{
-                "type": "Layout",
-                "direction": self._direction,
-                "gap": self._gap,
-                "children": self._children
-            }
-    def encoding(): return Encoding() 
-    def visualization(): return Visualization() 
-    def layout(): return Layout()
-    spec = layout().direction("vertical").gap("10px").add_child(
-        layout().direction("horizontal").gap("23px").add_child(
-            visualization()
-                .mark("line")
-                .add_encoding(
-                    encoding()
-                        .channel("x").field("Time")
-                        .channel("y").field("Distance")
-                )
+class Encoding:
+    def __init__(self):
+        self._channels = {}
+    def channel(self, name):
+        self._current_channel = name
+        return self
+    def field(self, fieldsname):
+        self._channels[self._current_channel] = fieldsname
+        self._current_channel = None
+        return self
+    def build(self):
+        return self._channels
+class Visualization:
+    def __init__(self):
+        self._mark = None
+        self._encoding = None
+    def mark(self, marktype):
+        self._mark = marktype
+        return self
+    def add_encoding(self, encoding):
+        self._encoding = encoding.build()
+        return self
+    def build(self):
+        return {
+            "type": "Visualization",
+            "mark": self._mark,
+            "encoding": self._encoding
+        }
+class Layout:
+    def __init__(self):
+        self._direction = None
+        self._gap = None
+        self._children = []
+    def direction(self, dirn):
+        self._direction = dirn
+        return self
+    def gap(self, gap):
+        self._gap = gap
+        return self
+    def add_child(self, *children):
+        for child in children:
+            self._children.append(child.build())
+        return self
+    def build(self):
+        return{
+            "type": "Layout",
+            "direction": self._direction,
+            "gap": self._gap,
+            "children": self._children
+        }
+def encoding(): return Encoding() 
+def visualization(): return Visualization() 
+def layout(): return Layout()
+json1 = layout().direction("vertical").gap("10px").add_child(
+    visualization()
+        .mark("bar")
+        .add_encoding(
+            encoding()
+                .channel("x").field("Temperature")
+                .channel("y").field("Weather")
+                .channel("color").field("blue")
         ),
+    layout().direction("horizontal").gap("30px").add_child(
         visualization()
-            .mark("bar")
+            .mark("line")
             .add_encoding(
                 encoding()
-                    .channel("x").field("Weather")
-                    .channel("y").field("People")
+                    .channel("x").field("Altitude")
+                    .channel("y").field("Air Pressure")
+                    .channel("color").field("orange")
+            )
+    ),
+)
+json2= layout().direction("horizontal").gap("14px").add_child(
+    visualization()
+        .mark("point")
+        .add_encoding(
+            encoding()
+                .channel("x").field("IQ")
+                .channel("y").field("Weight")
+                .channel("color").field("red")
+        ),
+    layout().direction("vertical").gap("33px").add_child(
+        visualization()
+            .mark("area")
+            .add_encoding(
+                encoding()
+                    .channel("x").field("Car Brand")
+                    .channel("y").field("percentage of Cars")
+                    .channel("color").field("green")
             )
     )
-    return json.dumps(spec.build(), indent=2)
+)
+def returnjson1():
+    return json.dumps(json1.build(), indent=2)
+def returnjson2():
+    return json.dumps(json2.build(), indent=2)
 function()
